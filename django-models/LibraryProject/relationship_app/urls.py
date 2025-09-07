05894fc6e33534b14613f30f4c49b7b41eb33e96
Onditi9 <omwegajoseph01@gmail.com>
2025-09-07 20:26:12 +0300

    Implemented authentication (register, login, logout) with templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, register
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
-    # Existing routes
-    path("books/", list_books, name="list_books"),   # function-based view
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),  # class-based view
+    # Function-based view
+    path("books/", list_books, name="list_books"),
 
-    # Authentication routes
+    # Class-based view
+    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+
+    # Authentication views
     path("register/", register, name="register"),
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
