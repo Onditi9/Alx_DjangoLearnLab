c6b0ab378786d36c4393821149f82a3a46e5535f
Onditi9 <omwegajoseph01@gmail.com>
2025-09-15 14:09:33 +0300

    api

--- api_project/serializers.py
+++ api_project/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
-from . import Book
+from .models import Book   # import your Book model
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = '__all__'   # include all fields from the model
+        fields = '__all__'   # include all model fields
+
