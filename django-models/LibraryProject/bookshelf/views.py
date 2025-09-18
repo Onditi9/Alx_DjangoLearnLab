37073d5eb423c7b94a7e9bd095b5921ab8f15934
Onditi9 <omwegajoseph01@gmail.com>
2025-09-18 17:30:14 +0300

    one

--- django-models/LibraryProject/bookshelf/views.py
+++ django-models/LibraryProject/bookshelf/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
 # Create your views here.
+from . import BookForm
+
+def create_book(request):
+    if request.method == "POST":
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return ("book_list")
+    else:
+        form = BookForm()
+    return (request, "bookshelf/form_example.html", {"form": form})
