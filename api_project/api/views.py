9fbc473b10393632f4837f5dece7295c184627c2
Onditi9 <omwegajoseph01@gmail.com>
2025-09-20 22:46:33 +0300

    project

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from .models import Book
 from . import BookSerializer
-
+from .serializers import BookSerializer
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
