1c5df3f1e0698cfe1add9217dd8f99ad05c9a8df
Onditi9 <omwegajoseph01@gmail.com>
2025-10-02 18:26:22 +0300

    form

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,56 +1,48 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm
 
-
 class PostListView(ListView):
-model = Post
-template_name = 'blog/post_list.html' # blog/templates/blog/post_list.html
-context_object_name = 'posts'
-paginate_by = 10
-
+    model = Post
+    template_name = "blog/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 10  # optional
 
 class PostDetailView(DetailView):
-model = Post
-template_name = 'blog/post_detail.html' # blog/templates/blog/post_detail.html
-context_object_name = 'post'
-
+    model = Post
+    template_name = "blog/post_detail.html"
 
 class PostCreateView(LoginRequiredMixin, CreateView):
-model = Post
-form_class = PostForm
-template_name = 'blog/post_form.html'
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    # redirect to list on success (or use get_absolute_url in model)
+    success_url = reverse_lazy("blog:post_list")
 
-
-def form_valid(self, form):
-# set the author before saving
-form.instance.author = self.request.user
-return super().form_valid(form)
-
+    def form_valid(self, form):
+        # set the current user as author before saving
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-model = Post
-form_class = PostForm
-template_name = 'blog/post_form.html'
+    model = Post
+    form_class = PostForm
+    template_name = "blog/post_form.html"
+    success_url = reverse_lazy("blog:post_list")
 
-
-def test_func(self):
-post = self.get_object()
-return post.author == self.request.user
-
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
-model = Post
-template_name = 'blog/post_confirm_delete.html'
-success_url = reverse_lazy('blog:post-list')
+    model = Post
+    template_name = "blog/post_confirm_delete.html"
+    success_url = reverse_lazy("blog:post_list")
 
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
-def test_func(self):
-post = self.get_object()
-return post.author == self.request.user                                                                                                                                                                                                                
+                                                                                                                                                                                                            
