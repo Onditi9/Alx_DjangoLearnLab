d4e2ec0d1ace04af16a0e702b0889adb59b39f1b
Onditi9 <omwegajoseph01@gmail.com>
2025-10-03 20:56:04 +0300

    forms

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,26 +1,18 @@
+# blog/models.py
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-
-
-User = get_user_model()
-
+from django.utils import timezone
+from django.contrib.auth.models import User   # ✅ Required for author field
 
 class Post(models.Model):
-title =  models.CharField(max_length=200)
-content = models.TextField()
-author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-created_at = models.DateTimeField(auto_now_add=True)
-updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)  # ✅ Link to User
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)  # ✅ Add published_date
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-class Meta:
-ordering = ['-created_at']
-
-
-def __str__(self):
-return self.title
-
-
-def get_absolute_url(self):
-return reverse('blog:post-detail', kwargs={'pk': self.pk})
+    def __str__(self):
+        return self.title
